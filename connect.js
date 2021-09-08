81a2dab206a603d6813c7a3fa9df8eb44017d449
htdhcvm <62516119+htdhcvm@users.noreply.github.com>
2021-09-08 22:34:04 +0300

    connect to db

---
+++ connect.js
@@ -0,0 +1,5 @@
+const postgres = require('postgres')
+
+const sql = postgres(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+
+module.exports = sql;
